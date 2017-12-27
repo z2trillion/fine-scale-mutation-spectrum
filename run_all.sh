a6f4faede17aae320b983e9adbfde24a4d6ba54d
Wayne Lee <wayne.lee@climate.com>
2017-12-28 09:33:58 +1100

    Add preprocessing to run script

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+cd preprocess
+python extract_diffs.py
+python gen_oneline_format.py
+cd ..
+
 python get_finescale_mut_spectra_nosingle.py 22;
 python get_finescale_phyloP_mut_type_v_freq_nosingle.py 22;
 python get_finescale_repeats_mut_type_v_freq_nosingle.py 22;
