297142b068452e8f7cf62b5e17a4a1e39b89aa52
Wayne Lee <wayne.lee@climate.com>
2017-12-28 09:26:03 +1100

    Loop preprocessing scripts over chromosomes

--- preprocess/extract_diffs.py
+++ preprocess/extract_diffs.py
@@ -1,76 +1,79 @@
-import sys
 import gzip
 
-chrom=sys.argv[1]
+def extract_diffs(chrom):
+    infile=gzip.open('../data/hg19_chimp_align/chr%i.hg19.panTro4.net.axt.gz' % chrom)
+    lines=infile.readlines()
+    infile.close()
 
-infile=gzip.open('chr'+chrom+'.hg19.panTro4.net.axt.gz')
-lines=infile.readlines()
-infile.close()
+    line_ind=0
+    while not lines[line_ind][0]=='0':
+        line_ind+=1
 
-line_ind=0
-while not lines[line_ind][0]=='0':
-    line_ind+=1
+    output='Pos SNP/Indel Human Chimp\n'
 
-output='Pos SNP/Indel Human Chimp\n'
-
-last_bin_start=0
-last_end=0
-while line_ind<len(lines):
-    s=lines[line_ind].split(' ')
-#    print s
-    start, end = int(s[2]), int(s[3])
-    if start>last_end+1:
-        output+=str(last_end)+' Indel '+str(start-last_end-1)+'\n'
-    last_end=end
-    if start>last_bin_start+10**6:
-        print start
-        last_bin_start=start
-    human=lines[line_ind+1].upper().strip('\n')
-    chimp=lines[line_ind+2].upper().strip('\n')
-    char_ind=0
-    while char_ind<len(human):
-        found_indel=False
-        h_allele=''
-        c_allele=''
-#        print human[char_ind], chimp[char_ind]
-        if human[char_ind]==chimp[char_ind] or human[char_ind]=='N' or chimp[char_ind]=='N':
-            char_ind+=1
-        elif human[char_ind] in 'ACGT' and chimp[char_ind] in 'ACGT':
-            found_match=False
-            h_allele=human[char_ind]
-            c_allele=chimp[char_ind]
-            char_ind+=1
-            while not found_match and not found_indel and char_ind<len(human):
-                if human[char_ind]==chimp[char_ind]:
-                    found_match=True
-                elif human[char_ind]=='-' or chimp[char_ind]=='-':
-                    found_indel=True
-                else:
-                    h_allele+=human[char_ind]
-                    c_allele+=chimp[char_ind]
-                    char_ind+=1
-            if not found_indel:
-                for i in range(len(h_allele)):
-                    j=char_ind-len(h_allele)+i
-                    output+=str(start+j)+' SNP '+human[j]+' '+chimp[j]+'\n'
-        if char_ind<len(human) and (human[char_ind]=='-' or chimp[char_ind]=='-'):
-            output+=str(start+char_ind)+' Indel '
-            len_indel=0
-            while char_ind<len(human) and not human[char_ind]==chimp[char_ind]:
-                if human[char_ind]=='-':
-                    start-=1
-#                h_allele+=human[char_ind]
-#                c_allele+=chimp[char_ind]
-                len_indel+=1
+    last_bin_start=0
+    last_end=0
+    while line_ind<len(lines):
+        s=lines[line_ind].split(' ')
+    #    print s
+        start, end = int(s[2]), int(s[3])
+        if start>last_end+1:
+            output+=str(last_end)+' Indel '+str(start-last_end-1)+'\n'
+        last_end=end
+        if start>last_bin_start+10**6:
+            # print start
+            last_bin_start=start
+        human=lines[line_ind+1].upper().strip('\n')
+        chimp=lines[line_ind+2].upper().strip('\n')
+        char_ind=0
+        while char_ind<len(human):
+            found_indel=False
+            h_allele=''
+            c_allele=''
+    #        print human[char_ind], chimp[char_ind]
+            if human[char_ind]==chimp[char_ind] or human[char_ind]=='N' or chimp[char_ind]=='N':
                 char_ind+=1
-            output+=str(len_indel)+'\n'
-#            output+=h_allele+' '+c_allele+'\n'
-#        if not h_allele=='':
-#            print h_allele, c_allele, end-(start+char_ind)
-    line_ind+=4
+            elif human[char_ind] in 'ACGT' and chimp[char_ind] in 'ACGT':
+                found_match=False
+                h_allele=human[char_ind]
+                c_allele=chimp[char_ind]
+                char_ind+=1
+                while not found_match and not found_indel and char_ind<len(human):
+                    if human[char_ind]==chimp[char_ind]:
+                        found_match=True
+                    elif human[char_ind]=='-' or chimp[char_ind]=='-':
+                        found_indel=True
+                    else:
+                        h_allele+=human[char_ind]
+                        c_allele+=chimp[char_ind]
+                        char_ind+=1
+                if not found_indel:
+                    for i in range(len(h_allele)):
+                        j=char_ind-len(h_allele)+i
+                        output+=str(start+j)+' SNP '+human[j]+' '+chimp[j]+'\n'
+            if char_ind<len(human) and (human[char_ind]=='-' or chimp[char_ind]=='-'):
+                output+=str(start+char_ind)+' Indel '
+                len_indel=0
+                while char_ind<len(human) and not human[char_ind]==chimp[char_ind]:
+                    if human[char_ind]=='-':
+                        start-=1
+    #                h_allele+=human[char_ind]
+    #                c_allele+=chimp[char_ind]
+                    len_indel+=1
+                    char_ind+=1
+                output+=str(len_indel)+'\n'
+    #            output+=h_allele+' '+c_allele+'\n'
+    #        if not h_allele=='':
+    #            print h_allele, c_allele, end-(start+char_ind)
+        line_ind+=4
 
-output+=str(last_end+1)+' Indel 100000000\n'
+    output+=str(last_end+1)+' Indel 100000000\n'
 
-outfile=open('human_chimp_diffs_chr'+chrom+'.txt','w')
-outfile.write(output)
-outfile.close()
+    outfile=open('../data/hg19_chimp_align/human_chimp_diffs_chr%i.txt' % chrom,'w')
+    outfile.write(output)
+    outfile.close()
+
+if __name__ == '__main__':
+    for chrom in range(1, 23):
+        extract_diffs(chrom)
+        print 'extracted diffs for chromosome %i' % chrom
--- preprocess/gen_oneline_format.py
+++ preprocess/gen_oneline_format.py
@@ -1,15 +1,20 @@
-import sys
+# import gzip
 
-chrom=sys.argv[1]
+def gen_oneline(chrom):
+    # infile=gzip.open('../data/hg19_reference/chr%i.fa.gz' % chrom)
+    infile=open('../data/hg19_reference/chr%i.fa' % chrom)
+    lines=infile.readlines()
+    infile.close()
 
-infile=open('chr'+chrom+'.fa')
-lines=infile.readlines()
-infile.close()
+    output=''
+    for line in lines[1:]:
+        output+=line.upper().strip('\n')
 
-output=''
-for line in lines[1:]:
-    output+=line.upper().strip('\n')
+    outfile=open('../data/hg19_reference/chr%i_oneline.txt' % chrom,'w')
+    outfile.write(output)
+    outfile.close()
 
-outfile=open('chr'+chrom+'_oneline.txt','w')
-outfile.write(output)
-outfile.close()
+if __name__ == '__main__':
+    for chrom in range(1, 23):
+        gen_oneline(chrom)
+        print 'generated oneline file for chromosome %i' % chrom
