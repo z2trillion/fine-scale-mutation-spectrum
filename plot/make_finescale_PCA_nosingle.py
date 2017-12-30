5e8075e5bdce03fabb2cb66b597e084ad4425b2f
Wayne Lee <wayne.lee@climate.com>
2017-12-30 18:01:42 +1300

    Add args for PCA plotter

--- plot/make_finescale_PCA_nosingle.py
+++ plot/make_finescale_PCA_nosingle.py
@@ -22,7 +22,6 @@ for (b2,d) in [('A','T'),('A','C'),('A','G'),('C','T'),('C','G'),('C','A')]:
             ylabel.append(b2+r'$\to$'+d+r'  '+b1)
         else:
             ylabel.append(b1)
-#        ylabel.append('5\'-'+b1+b2+r'$\to$'+d)
         for b3 in 'ACGT':
             mut_index[(b1+b2+b3,d)]=(row,col)
             inv_mut_index[(row,col)]=b1+b2+b3+'_'+d
@@ -30,11 +29,7 @@ for (b2,d) in [('A','T'),('A','C'),('A','G'),('C','T'),('C','G'),('C','A')]:
             col+=1
         row+=1
 
-infile=open('../finescale_mut_spectra/derived_each_lineage_chr22_nosingle.txt')
-lines=infile.readlines()
-s=lines[0].strip('\n').split(' ')
-
-groups=['EUR','EAS','SAS','AFR']
+# groups=['EUR','EAS','SAS','AFR']
 groups=['EUR','EAS','SAS','AFR','AMR']
 pops=dict({})
 pops['EUR']=['CEU','GBR','FIN','IBS','TSI']
@@ -60,88 +55,120 @@ for group in groups:
     for pop in pops[group]:
         bigpop[pop]=group
 
-indices=dict({})
-for group in groups:
-    indices[group]=[]
-    for pop in pops[group]:
-        indices[pop]=[]
-
-pop_this_index=dict({})
-for i in range(1,len(s)):
-    pop_this_index[i]=s[i]
-    indices[s[i]].append(i-1)
-    indices[bigpop[s[i]]].append(i-1)
-
-mut_counts=np.zeros((2*(len(s)-1),len(lines)-1))
-print len(mut_counts),len(mut_counts[0])
-
-mut_list=[]
-for chrom in range(1,23):
-    infile=open('../finescale_mut_spectra/derived_each_lineage_chr'+str(chrom)+'_nosingle.txt')
+def make_plots(chromosomes, groups_asdfasdf):
+    infile=open('../finescale_mut_spectra/derived_each_lineage_chr%i_nosingle.txt' % chromosomes[0])
     lines=infile.readlines()
-    infile.close()
+    s=lines[0].strip('\n').split(' ')
 
-    for i in range(len(lines)-1):
-        s=lines[i+1].strip('\n').split(' ')
-        if chrom==1:
-            mut_list.append(s[0])
-        for j in range(len(s)-1):
-#            print j,i,len(s)
-            mut_counts[j][i]+=int(s[j+1])
-for j in range(len(s)-1):
-    der_count=mut_counts[j].sum()
-    for i in range(len(mut_counts[j])):
-        mut_counts[j][i]*=1.0/der_count
+    indices=dict({})
+    for group in groups:
+        indices[group]=[]
+        for pop in pops[group]:
+            indices[pop]=[]
 
-averaged_mut_counts=[]
-for j in range((len(s)-1)/2):
-    averaged_mut_counts.append([])
-    for i in range(len(mut_counts[0])):
-        averaged_mut_counts[-1].append(0.5*(mut_counts[2*j][i]+mut_counts[2*j+1][i]))
-mut_counts=np.array(averaged_mut_counts)
+    pop_this_index=dict({})
+    for i in range(1,len(s)):
+        pop_this_index[i]=s[i]
+        indices[s[i]].append(i-1)
+        indices[bigpop[s[i]]].append(i-1)
 
-for group in groups:
-    print group
-    group_mut_counts=[]
-    for i in indices[group]:
-        group_mut_counts.append(mut_counts[i])
-    group_mut_counts=np.array(group_mut_counts)
-    myPCA=PCA(group_mut_counts)
+    mut_counts=np.zeros((2*(len(s)-1),len(lines)-1))
+    print len(mut_counts),len(mut_counts[0])
 
-    colors=['blue','green','red','purple','black','orange']
-    for i in range(len(pops[group])):
-        x,y=[],[]
-        for ind in indices[pops[group][i]]:
-            this_point=myPCA.project(mut_counts[ind])
-            x.append(this_point[0])
-            y.append(this_point[1])
-        plt.scatter(x,y,color=colors[i],label=longname[pops[group][i]])
-    plt.legend(loc='lower left',ncol=2,prop={'size':8})
-    plt.xticks(())
-    plt.yticks(())
-    plt.xlabel('PC1 ('+str(int(100*myPCA.fracs[0]))+'% variance explained)')
-    plt.ylabel('PC2 ('+str(int(100*myPCA.fracs[1]))+'% variance explained)')
-    fig=plt.gcf()
-    fig.set_size_inches((4.5,3.5))
-    plt.savefig(group+'_mut_PCA_1kg_nosingle_altlegend.pdf')
-    plt.clf()
+    mut_list=[]
+    for chrom in chromosomes:
+        infile=open('../finescale_mut_spectra/derived_each_lineage_chr'+str(chrom)+'_nosingle.txt')
+        lines=infile.readlines()
+        infile.close()
 
-    PC_vector=myPCA.Wt
+        for i in range(len(lines)-1):
+            s=lines[i+1].strip('\n').split(' ')
+            if chrom==1:
+                mut_list.append(s[0])
+            for j in range(len(s)-1):
+                mut_counts[j][i]+=int(s[j+1])
+    for j in range(len(s)-1):
+        der_count=mut_counts[j].sum()
+        for i in range(len(mut_counts[j])):
+            mut_counts[j][i]*=1.0/der_count
 
-    for j in range(2):
-        PC_grid=np.zeros((row,col))
-        for i in range(len(mut_list)):
-            mut=mut_list[i]
-            PC_grid[mut_index[(mut[:3],mut[4])]]-=float(PC_vector[j][i])
-        plt.xticks((0.5,1.5,2.5,3.5),('3\' -A', 'C','G','T'))
-        plt.yticks(tuple(ypos),tuple(ylabel))
-        for i in [4,8,12,16,20]:
-            plt.axhline(y=i,color='black')
-        plt.pcolor(PC_grid,cmap='seismic',vmin=-0.45,vmax=0.45)
-        plt.colorbar()
+    averaged_mut_counts=[]
+    for j in range((len(s)-1)/2):
+        averaged_mut_counts.append([])
+        for i in range(len(mut_counts[0])):
+            averaged_mut_counts[-1].append(0.5*(mut_counts[2*j][i]+mut_counts[2*j+1][i]))
+    mut_counts=np.array(averaged_mut_counts)
+
+
+
+    for group in groups:
+        print group
+        group_mut_counts=[]
+        for i in indices[group]:
+            group_mut_counts.append(mut_counts[i])
+        group_mut_counts=np.array(group_mut_counts)
+        myPCA=PCA(group_mut_counts)
+
+        colors=['blue','green','red','purple','black','orange']
+        for i in range(len(pops[group])):
+            x,y=[],[]
+            for ind in indices[pops[group][i]]:
+                this_point=myPCA.project(mut_counts[ind])
+                x.append(this_point[0])
+                y.append(this_point[1])
+            plt.scatter(x,y,color=colors[i],label=longname[pops[group][i]])
+        plt.legend(loc='lower left',ncol=2,prop={'size':8})
+        plt.xticks(())
+        plt.yticks(())
+        plt.xlabel('PC1 ('+str(int(100*myPCA.fracs[0]))+'% variance explained)')
+        plt.ylabel('PC2 ('+str(int(100*myPCA.fracs[1]))+'% variance explained)')
         fig=plt.gcf()
-        fig.set_size_inches((4,6))
-        plt.title('Principal Component '+str(j+1))
-        plt.tight_layout()
-        plt.savefig(group+'_mut_PC'+str(j)+'_nosingle.pdf',format='pdf')
+        fig.set_size_inches((4.5,3.5))
+        plt.savefig(group+'_mut_PCA_1kg_nosingle_altlegend.pdf')
         plt.clf()
+        #
+        # PC_vector=myPCA.Wt
+        #
+        # for j in range(2):
+        #     PC_grid=np.zeros((row,col))
+        #     for i in range(len(mut_list)):
+        #         mut=mut_list[i]
+        #         PC_grid[mut_index[(mut[:3],mut[4])]]-=float(PC_vector[j][i])
+        #     plt.xticks((0.5,1.5,2.5,3.5),('3\' -A', 'C','G','T'))
+        #     plt.yticks(tuple(ypos),tuple(ylabel))
+        #     for i in [4,8,12,16,20]:
+        #         plt.axhline(y=i,color='black')
+        #     plt.pcolor(PC_grid,cmap='seismic',vmin=-0.45,vmax=0.45)
+        #     plt.colorbar()
+        #     fig=plt.gcf()
+        #     fig.set_size_inches((4,6))
+        #     plt.title('Principal Component '+str(j+1))
+        #     plt.tight_layout()
+        #     plt.savefig(group+'_mut_PC'+str(j)+'_nosingle.pdf',format='pdf')
+        #     plt.clf()
+
+def Group(group):
+    if group in bigpop:
+        return [group]
+    try:
+        return pops[group]
+    except KeyError:
+        raise ValueError
+
+
+if __name__ == '__main__':
+    import sys
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--chromosomes', type=int, nargs='+',
+                        default=range(1, 23))
+    parser.add_argument('-g', '--groups', type=Group, nargs='+')
+
+    args = parser.parse_args(sys.argv[1:])
+    chromosomes = args.chromosomes
+    for chrom in chromosomes:
+        assert 1 <= chrom and chrom <= 22, ('Chromosome %i is unlikely to exist'
+                                            % chrom)
+
+    make_plots(chromosomes, args.groups)
