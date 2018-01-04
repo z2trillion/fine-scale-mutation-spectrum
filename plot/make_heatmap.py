7813aa528676e0cf3aa6c9af416a2576b83dcc5d
z2trillion <wayne.lee@climate.com>
2018-01-05 01:39:00 +1300

    Fix chromosomes bug, add plot and column titles, and set plot limits

--- plot/make_heatmap.py
+++ plot/make_heatmap.py
@@ -37,7 +37,7 @@ for (b2,d) in [('A','T'),('A','C'),('A','G'),('C','T'),('C','G'),('C','A')]:
 def frequency_breakdown(path, chromosomes, frequency_range):
     count_array=np.zeros((row,col))
     for chrom in chromosomes:
-        infile=open(path)
+        infile=open(path % str(chrom))
         lines=infile.readlines()
         infile.close()
 
@@ -62,13 +62,13 @@ def heatmap(chromosomes, population_pair, frequency_range, exclude):
     num_variants = {}
 
     for pop in population_pair:
-        path = '../finescale_mut_spectra/mut_type_v_allele_freq_'+pop+'_chr'+str(chromosome)+'_nosingle.txt'
+        path = '../finescale_mut_spectra/mut_type_v_allele_freq_'+pop+'_chr%s_nosingle.txt'
         pop_counts[pop]=frequency_breakdown(path, chromosomes, frequency_range)
         if exclude:
-            repeats_path = '../finescale_mut_spectra/inrepeats_mut_type_v_allele_freq_'+pop+'_chr'+str(chromosome)+'_nosingle.txt'
+            repeats_path = '../finescale_mut_spectra/inrepeats_mut_type_v_allele_freq_'+pop+'_chr%s_nosingle.txt'
             pop_counts[pop] -= frequency_breakdown(repeats_path, chromosomes, frequency_range)
 
-            conserved_path = '../finescale_mut_spectra/phyloP_conserved_mut_type_v_allele_freq_'+pop+'_chr'+str(chromosome)+'_nosingle.txt'
+            conserved_path = '../finescale_mut_spectra/phyloP_conserved_mut_type_v_allele_freq_'+pop+'_chr%s_nosingle.txt'
             pop_counts[pop] -= frequency_breakdown(conserved_path, chromosomes, frequency_range)
         num_variants[pop]=pop_counts[pop].sum()
 
@@ -85,12 +85,45 @@ def heatmap(chromosomes, population_pair, frequency_range, exclude):
                 sig_x.append(j+0.5)
                 sig_y.append(i+0.5)
 
-    title = ' vs. '.join(population_pair)
-    title += ' chromosome(s) ' + ' '.join([str(c) for c in chromosomes])
+    return ratio_grid, (sig_x, sig_y)
 
-    return ratio_grid, (sig_x, sig_y), title
+def make_titles(chromosome_groups, population_pairs, frequency_range, exclude):
+    title = ''
 
-def make_plot(ratio_grids, significant_indices, titles):
+    distinct_population_pairs = len(set(population_pairs)) != 1
+    if not distinct_population_pairs:
+        title += '/'.join(population_pairs[0])
+
+    distinct_chromosome_groups = (
+        len(set(tuple(cg) for cg in chromosome_groups)) != 1
+    )
+    if not distinct_chromosome_groups:
+        if set(chromosome_groups[0]) == set(str(c) for c in range(1, 23)):
+            title += ' Autosomes'
+        elif len(chromosome_groups[0]) == 1:
+            title += ' Chromosome %s' % str(chromosome_groups[0][0])
+        else:
+            title += ' Chromosomes %s' % ' '.join(str(c)
+                                                  for c in chromosome_groups[0])
+    # if exclude:
+    #     title += '\nExcluding Repeats and Conserved Regions'
+    if frequency_range != [0, 1]:
+        title += ' %.2f <= f <= %.2f' % tuple(frequency_range)
+
+    column_titles = []
+    for chromosomes, population_pair in product(chromosome_groups,
+                                                population_pairs):
+        column_title = ''
+        if distinct_population_pairs:
+            column_title += '/'.join(population_pair)
+        if distinct_chromosome_groups:
+            column_title += ' '
+            column_title += ' '.join(str(c) for c in chromosomes)
+        column_titles.append(column_title)
+
+    return title, column_titles
+
+def make_plot(ratio_grids, significant_indices, plot_title, column_titles):
     combined_grids = np.hstack(ratio_grids)
     r = max(1 - np.min(combined_grids), np.max(combined_grids) - 1)
     plt.pcolor(np.hstack(ratio_grids), vmin=1 - r,vmax=1 + r, cmap='seismic')
@@ -107,6 +140,10 @@ def make_plot(ratio_grids, significant_indices, titles):
     for k in range(0, len(ratio_grids[0]), 4):
         plt.axhline(y=k,color='black', linestyle='dashed')
 
+    top_x_axis = plt.twiny()  # Create a twin Axes sharing the yaxis, lol
+    top_x_axis.set_xticks(np.arange(2, 4 * len(ratio_grids), 4))
+    top_x_axis.set_xticklabels(column_titles)
+
     combined_x_positions = []
     combined_y_positions = []
     for i, (x_positions, y_positions) in enumerate(significant_indices):
@@ -116,8 +153,13 @@ def make_plot(ratio_grids, significant_indices, titles):
     plt.scatter(combined_x_positions, combined_y_positions, marker='.',
                 color='white')
 
+    plt.xlim(0, len(combined_grids[0]))
+    plt.ylim(0, len(combined_grids))
+
+    plt.title(plot_title + '\n')
+
     plt.gcf()
-    plt.savefig('testestest.pdf',format='pdf')
+    plt.savefig('heatmap.pdf',format='pdf')
     plt.clf()
 
 valid_populations = [
@@ -140,8 +182,8 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--chromosomes', type=int, nargs='+',
-                        default=range(1, 23))
+    parser.add_argument('-c', '--chromosomes', type=str, nargs='+',
+                        default=[str(c) for c in range(1, 23)])
     parser.add_argument('-i', '--individually', action='store_true')
     parser.add_argument('-p', '--population-pairs', type=Population,
                         nargs='+')
@@ -152,7 +194,7 @@ if __name__ == '__main__':
 
     chromosomes = args.chromosomes
     for chromosome in chromosomes:
-        assert chromosome in range(1, 23) + ['X']
+        assert chromosome == 'X' or int(chromosome) in range(1, 23)
     if args.individually:
         chromosome_groups = [[chromosome] for chromosome in chromosomes]
     else:
@@ -168,4 +210,10 @@ if __name__ == '__main__':
         for chromosomes, population_pair in product(chromosome_groups,
                                                     population_pairs)
     ]
-    make_plot(*zip(*heatmaps))
+
+    ratio_grids, significant_indices = zip(*heatmaps)
+
+    plot_title, column_titles = make_titles(chromosome_groups, population_pairs,
+                                            frequency_range, exclude)
+
+    make_plot(ratio_grids, significant_indices, plot_title, column_titles)
